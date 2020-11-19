b3af9ccbfc37fe8a133cb51fba6dfc039507a7d8
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-11-19 12:38:32 +0300

    Delete inheritance_radar.c

--- inheritance_radar.c
+++
@@ -1,9 +0,0 @@
-#include "inheritance_radar.h"
-
-void radar_constructor (radar * const me, uint16_t init_phase, uint16_t init_att, uint16_t init_volt, uint16_t init_freq)
-{
-	antenna_constructor(&me->super, init_phase, init_att);
-
-	me->voltage = init_volt;
-	me->frequency = init_freq;
-}
